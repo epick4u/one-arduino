5ec47e4499548e739642e6f1e709e3f3314f4740
정승길/Onestore/SKP <sg.jueng@sk.com>
2016-06-23 11:14:51 +0900

    프로젝트 등록

---
+++ simple-rest/src/main/java/com/onelab/arduino/sample/RoomVO.java
@@ -0,0 +1,22 @@
+package com.onelab.arduino.sample;
+
+import java.util.Date;
+
+public class RoomVO {
+	
+	private String id;
+	private Date startTime;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public Date getStartTime() {
+		return startTime;
+	}
+	public void setStartTime(Date startTime) {
+		this.startTime = startTime;
+	}
+}
---
+++ simple-rest/src/main/resources/static/js/controllers.js
@@ -0,0 +1,7 @@
+angular.module('app.controllers', [])
+     
+.controller('page4Ctrl', function($scope) {
+	$scope.roomState = {};
+	$scope.roomState['13-1'] = "사용중";
+})
+ 
