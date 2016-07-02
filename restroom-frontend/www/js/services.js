c7ace26d57722024ab67b26957b921d22a153b8b
Dale Seo <dale.seo@gmail.com>
2016-07-02 12:43:44 +0900

    settings mvc

--- restroom-frontend/www/js/services.js
+++ restroom-frontend/www/js/services.js
@@ -2,8 +2,10 @@ const baseUrl = "http://ec2-52-78-61-81.ap-northeast-2.compute.amazonaws.com:808
 
 angular.module('app.services', [])
 
-.service('mainSvc', function($q, $http) {
-  this.update = function(id, restroom) {
+.service('SettingsSvc', function($http) {
+  var mainSvc = this;
+
+  mainSvc.update = function(id, restroom) {
     console.log('>> Calling mainSvc#update');
 
     var action = restroom.occupied ? 'in' : 'out';
@@ -17,32 +19,26 @@ angular.module('app.services', [])
   };
 
   // TODO Doesn' work because of async
-  this.list = function(floor) {
+  mainSvc.list = function(floor) {
     console.log('>> Calling mainSvc#list');
 
-    var restrooms = [];
-    var deferred = $q.defer();
-    $http.get(baseUrl).success(function(data) {
-      restrooms = data.filter(function(restroom) {
-        if (!floor) {
-          return true;
-        }
-        return restroom.id.split('-')[0] == floor;
-      }).sort(function(a, b) {
-        if (a.id > b.id) {
-          return 1;
-        }
-        if (a.id < b.id) {
-          return -1;
-        }
-        return 0;
+    return $http.get(baseUrl)
+      .then(function(result) {
+        return result.data.filter(function(restroom) {
+          if (!floor) {
+            return true;
+          }
+          return restroom.id.split('-')[0] == floor;
+        }).sort(function(a, b) {
+          if (a.id > b.id) {
+            return 1;
+          }
+          if (a.id < b.id) {
+            return -1;
+          }
+          return 0;
+        });
       });
-      deferred.resolve(data);
-    }).error(function(data, status, headers) {
-      alert('Repos status ' + status + ' --- headers : ' + headers);
-    });
-
-    return restrooms;
   };
 
 });
